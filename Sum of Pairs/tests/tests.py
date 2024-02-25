4769803568ff3c663e4edca0fe1966f6fdba8ce1
David Padilla <109883212+Davidpm02@users.noreply.github.com>
2024-02-25 01:18:10 +0100

    Add files via upload

---
+++ Sum of Pairs/tests/tests.py
@@ -0,0 +1,8 @@
+import sys
+import os
+import random
+
+# Agrego el directorio padre al sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import scripts.challenge as challenge
