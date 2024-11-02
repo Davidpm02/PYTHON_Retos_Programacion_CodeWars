5eefb4e1e7648a0d65d0b8f6ca73b11457a1fdfa
Davidpm02 <davidpamu02@gmail.com>
2024-11-02 23:59:50 +0100

    Eliminación de retos incompletos (limpieza).

--- Training on Pete, the baker/tests/test.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-import random
-
-# Agrego el directorio padre al sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-import scripts.challenge as challenge
